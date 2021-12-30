b5a269be25b6cfc5c4e53d76e5aa5ab350a5d819
Ashish9953 <76690338+Ashish9953@users.noreply.github.com>
2021-12-30 10:02:27 +0530

    Add files via upload

---
+++ models/credential.js
@@ -0,0 +1,39 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Credential extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Credential.init(
+    {
+      emailId: { type: DataTypes.STRING, allowNull: false },
+      userId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: "userDetails",
+          },
+          key: "id",
+        },
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Credential",
+    }
+  );
+  Credential.associate = function (models) {
+    Credential.belongsTo(models.userDetails, {
+      foreignKey: "userId",
+    });
+  };
+  return Credential;
+};
